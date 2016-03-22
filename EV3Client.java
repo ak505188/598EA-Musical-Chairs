b8aa29eb1de8348ab244e5013ed2552aa3be56c5
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2016-03-22 01:29:35 -0700

    Cleaned up the code on connection.

--- EV3Client.java
+++ EV3Client.java
@@ -3,9 +3,25 @@ import java.net.ServerSocket;
 
 public class EV3Client {
 
- public static void main (String[] args) throws Exception {
-    Socket s = new Socket("10.0.1.1", 1111);
-    System.out.println("Client should be connected?");
-
+  public static void main(String[] args) {
+    if (args.length != 2) {
+      System.out.println("Insert IP address & port number as arguments");
+      System.out.println("Exiting now");
+      System.exit(0);
+    }
+    connectToEV3(args[0], Integer.parseInt(args[1]));
   }
+
+  public static void connectToEV3(String ipAddress, int portNumber) {
+    try {
+      Socket s = new Socket(ipAddress, portNumber);
+      System.out.println("Client connected successfully.");
+    } catch (Exception e) {
+      System.err.println("Error: " + e);
+      System.err.println("Exiting now.");
+      System.exit(0);
+    }
+  }
+
 }
+
--- EV3Server.java
+++ EV3Server.java
@@ -1,12 +1,15 @@
 import java.net.Socket;
 import java.net.ServerSocket;
 
+import lejos.hardware.Sound;
+
 public class EV3Server {
 
  public static void main (String[] args) throws Exception {
     ServerSocket serv = new ServerSocket(1111);
+    // Waits for client connection
     Socket s = serv.accept();
-    System.out.println("Server should be waiting right now.");
+    System.out.println("Server connected successfully.");
 
   }
 }
