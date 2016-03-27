aa6addb02a309429406875e6eb334afe41bc6797
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2016-03-27 15:56:20 -0700

    It can turn now! More intuitive controls.

--- EV3Server.java
+++ EV3Server.java
@@ -2,29 +2,34 @@ import java.net.Socket;
 import java.net.ServerSocket;
 import java.io.DataInputStream;
 import lejos.hardware.Sound;
-import lejos.hardware.motor.Motor;
+import lejos.hardware.motor.*;
+import lejos.hardware.port.MotorPort;
 
 public class EV3Server {
+  public static EV3LargeRegulatedMotor rightWheel = new EV3LargeRegulatedMotor(MotorPort.A);
+  public static EV3LargeRegulatedMotor leftWheel = new EV3LargeRegulatedMotor(MotorPort.D);
 
   public static void main (String[] args) throws Exception {
+    System.out.println("Server ready for connection.");
     ServerSocket serv = new ServerSocket(1111);
     // Waits for client connection
     Socket s = serv.accept();
     System.out.println("Server connected successfully.");
     DataInputStream inStream = new DataInputStream(s.getInputStream());
-    Motor.A.rotateTo(90);
-    Motor.B.setSpeed(Motor.B.getMaxSpeed());
+    // Motor.A.rotateTo(90);
     while (true) {
       String action = inStream.readUTF();
       System.out.println(action);
-      if (action.equals("a")) {
-        hammerTime();
-      } else if (action.equals("b")) {
-        Motor.B.forward();
-      } else if (action.equals("c")) {
-        Motor.B.stop();
+      if (action.equals("w")) {
+        driveForward();
+      } else if (action.equals("s")) {
+        stopDriving();
+      } else if (action.equals("a")) {
+        driveLeft();
       } else if (action.equals("d")) {
-        Motor.B.backward();
+        driveRight();
+      } else if (action.equals("x")) {
+        driveBackward();
       } else {
         Sound.beep();
       }
@@ -38,4 +43,36 @@ public class EV3Server {
       Motor.A.rotateTo(0);
     }
   }
+
+  public static void driveForward() {
+    leftWheel.setSpeed(leftWheel.getMaxSpeed());
+    rightWheel.setSpeed(rightWheel.getMaxSpeed());
+    leftWheel.backward();
+    rightWheel.backward();
+  }
+
+  public static void stopDriving() {
+    leftWheel.stop();
+    rightWheel.stop();
+  }
+
+  public static void driveBackward() {
+    leftWheel.forward();
+    rightWheel.forward();
+  }
+
+  public static void driveLeft() {
+    leftWheel.setSpeed((float)(leftWheel.getMaxSpeed() / 5.0 ));
+    leftWheel.backward();
+    rightWheel.setSpeed(rightWheel.getMaxSpeed());
+    rightWheel.backward();
+  }
+
+  public static void driveRight() {
+    rightWheel.setSpeed((float)(rightWheel.getMaxSpeed() / 5.0));
+    rightWheel.backward();
+    leftWheel.setSpeed(leftWheel.getMaxSpeed());
+    leftWheel.backward();
+  }
+
 }
